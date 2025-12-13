9bcf40d5640c0c29f9b3cf7e0dfc417fa94ad2d2
David COBAC <david.cobac@ac-nantes.fr>
2025-12-13 16:55:14 +0100

    various themes + image

--- ghcw.py
+++ ghcw.py
@@ -9,6 +9,7 @@ Version: 1.1
 Python Version: 3.13
 Dependencies: aiohttp, libqtile
 License: GNU GPL Version 3
+Repo: https://github.com/cobacdavid/my_qtile_widgets
 """
 
 import asyncio
@@ -37,6 +38,35 @@ query($login: String!, $from: DateTime!, $to: DateTime!) {
 }
 """
 
+THEMES = {'light': [[1 - (.25*i)**.5]*3 for i in range(5)],
+          'dark': [[(.25*i)**.5]*3 for i in range(5)],
+          'gh': [(.9, .9, .9),
+                 (.77, .89, .55),
+                 (.48, .79, .44),
+                 (.14, .60, .23),
+                 (.1, .38, .15)],
+          'ghd': [(.09, .11, .13),
+                  (0, .22, .13),
+                  (0, .37, .18),
+                  (.06, .60, .24),
+                  (.15, .84, .27)],
+          'xmas1': [(.97, .91, .88),
+                    (.58, .48, .44),
+                    (.43, .20, .11),
+                    (.20, .21, .18),
+                    (.13, .03, .04)],
+          'xmas2': [(.95, .82, .68),
+                    (.93, .62, .53),
+                    (.94, .23, .24),
+                    (.63, .05, .09),
+                    (.21, .02, .03)],
+          'xmas3': [(.85, .90, .95),
+                    (.16, .42, .63),
+                    (.67, .78, .66),
+                    (.48, .61, .39),
+                    (.33, .37, .25)],
+          }
+
 
 class Contrib_day:
     def __init__(self, x, y, dim, couleur):
@@ -54,14 +84,14 @@ class Contrib_day:
         ctx.restore()
 
 
-class Ghcw(base._Widget, base.MarginMixin):
+class Ghcw(base._Widget):
     defaults = [
-        ("idgithub", "cobacdavid", ""),
+        ("colors", None, ""),
+        ("empty_cell_color", None, ""),
         ("gap", None, ""),
+        ("idgithub", "cobacdavid", ""),
         ("nweeks", 52, ""),
-        ("colors",
-         [(.1, .1, .1)] + [(0, .2*i, 0) for i in range(1, 6)],
-         "")
+        ("theme", "gh", "")
     ]
 
     def __init__(self, token, **config):
@@ -147,17 +177,22 @@ class Ghcw(base._Widget, base.MarginMixin):
 
         self.dim = (self.bar.height - 8 * self.gap) // 7
         step = self.dim + self.gap
-        lgth = self.nweeks*step + self.gap
+        lgth = self.nweeks*step - self.gap
         hght = 7*self.dim + 8*self.gap
 
         self.drawer.clear(self.background or self.bar.background)
         ctx = self.drawer.ctx
         ctx.translate(0, (self.bar.height - hght) / 2)
 
+        if not self.colors:
+            self.colors = THEMES[self.theme]
+        if self.empty_cell_color:
+            self.colors[0] = self.empty_cell_color
+
         for col in range(self.nweeks):
             for lig in range(7):
                 intensity = self._tab_donnees[col * 7 + lig][1]
-                couleur = self.colors[min(5, int(intensity))]
+                couleur = self.colors[min(4, int(intensity))]
                 Contrib_day(col*step,
                             self.gap + lig*step,
                             self.dim, couleur).draw(ctx)
